3ae8986e5d213cfac8fe3e43b935131b4ce63ba3
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-08-17 19:50:25 +0200

    dedupe code in unit tests

--- test/DiscovergyMeter.js
+++ test/DiscovergyMeter.js
@@ -5,16 +5,27 @@ describe('DiscovergyMeter Node', function () {
     afterEach(function () {
       helper.unload();
     });
+    let n1 = {
+      id: "n1",
+      type: "Discovergy Meter",
+      name: "test name",
+      username: "demo@corrently.de",
+      password: "aNPR66nGXQhZ",
+      meterId: '781ffa307e434529be9f747eece1b8dc'
+    }
 
+    const testBasicPropertiesOfMsg = function(msg) {
+      msg.should.have.property('payload');
+      msg.payload.should.have.property('time');
+      msg.payload.should.have.property('latest');
+      msg.payload.latest.should.have.property('power');
+      msg.payload.latest.should.have.property('energy_wh');
+      msg.payload.latest.should.have.property('energyOut_wh');
+      msg.payload.latest.should.have.property('baseCosts');
+      msg.payload.latest.should.have.property('energyCost');
+    }
     it('should be loaded', function (done) {
-        var flow = [{
-            id: "n1",
-            type: "Discovergy Meter",
-            name: "test name",
-            username: "demo@corrently.de",
-            password: "aNPR66nGXQhZ",
-            meterId: '781ffa307e434529be9f747eece1b8dc'
-          }];
+        var flow = [n1];
         helper.load(dgyMeterNode, flow, function () {
           var n1 = helper.getNode("n1");
           n1.should.have.property('name', 'test name');
@@ -24,30 +35,16 @@ describe('DiscovergyMeter Node', function () {
 
     it('should retrieve single meter measurements', function (done) {
       this.timeout(19000);
+      n1.wires=[["n2"]];
       var flow = [
-        {
-            id: "n1",
-            type: "Discovergy Meter",
-            name: "test name",
-            username: "demo@corrently.de",
-            password: "aNPR66nGXQhZ",
-            meterId: '781ffa307e434529be9f747eece1b8dc',
-            wires:[["n2"]]
-        },
+      n1,
         { id: "n2", type: "helper" }
       ];
       helper.load(dgyMeterNode, flow, function () {
         var n2 = helper.getNode("n2");
         var n1 = helper.getNode("n1");
         n2.on("input", function (msg) {
-          msg.should.have.property('payload');
-          msg.payload.should.have.property('time');
-          msg.payload.should.have.property('latest');
-          msg.payload.latest.should.have.property('power');
-          msg.payload.latest.should.have.property('energy_wh');
-          msg.payload.latest.should.have.property('energyOut_wh');
-          msg.payload.latest.should.have.property('baseCosts');
-          msg.payload.latest.should.have.property('energyCost');
+          testBasicPropertiesOfMsg(msg);
           done();
         });
         n1.receive({ payload: 'Wir lieben Corrently Ökostrom' });
@@ -55,16 +52,9 @@ describe('DiscovergyMeter Node', function () {
   });
   it('should retrieve two meter measurements (aggregation)', function (done) {
     this.timeout(19000);
+    n1.wires=[["n3"]];
     var flow = [
-      {
-          id: "n1",
-          type: "Discovergy Meter",
-          name: "test name",
-          username: "demo@corrently.de",
-          password: "aNPR66nGXQhZ",
-          meterId: '781ffa307e434529be9f747eece1b8dc',
-          wires:[["n3"]]
-      },
+      n1,
       {
           id: "n3",
           type: "Discovergy Meter",
@@ -80,14 +70,7 @@ describe('DiscovergyMeter Node', function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       n2.on("input", function (msg) {
-        msg.should.have.property('payload');
-        msg.payload.should.have.property('time');
-        msg.payload.should.have.property('latest');
-        msg.payload.latest.should.have.property('power');
-        msg.payload.latest.should.have.property('energy_wh');
-        msg.payload.latest.should.have.property('energyOut_wh');
-        msg.payload.latest.should.have.property('baseCosts');
-        msg.payload.latest.should.have.property('energyCost');
+        testBasicPropertiesOfMsg(msg);
         msg.payload.should.have.property('aggregation');
         done();
       });
